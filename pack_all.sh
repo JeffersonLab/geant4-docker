722daf1d673259525d61326104ad3e03c2af9caf
mungaro <maurizio.ungaro@cnu.edu>
2024-06-24 11:38:01 -0400

    updated packing scripts

--- pack_all.sh
+++ pack_all.sh
@@ -3,20 +3,22 @@
 #osnames=(almalinux93 )
 osnames=(fedora36 almalinux93 ubuntu24)
 osnames=(fedora36 almalinux93)
+osnames=(ubuntu24)
 g4_versions_not_in_clas12=()
 g4_versions_not_in_clas12=(11.2.2)
 clas12tags_docker_tags=(dev) # see conventions.py for details
 clas12tags_docker_tags=(prod1 dev) # see conventions.py for details
 local_install_dir=/Users/ungaro/mywork
-container_install_dir=/Users/ungaro/mywork
+container_install_dir=/usr/local/mywork/
 remote_images_location=/work/clas12/ungaro/images
-
 
 install_dirs=(cvmfs)
 
-gemc_only=""
+what_to_pack="all"
 if [ $1 = "gemc" ]; then
-	gemc_only="yes"
+	what_to_pack="gemc"
+elif  [ $1 = "sim" ]; then
+	what_to_pack="sim"
 fi
 
 function g4_version_from_clas12tags_version {
@@ -39,38 +41,38 @@ function do_the_deed {
 	gemc_or_sim=$2
 	remote_image_name="jeffersonlab/$gemc_or_sim:$image_name"
 	echo "$image_name  remote: $remote_image_name"
-	docker pull "$remote_image_name"
+	# docker pull "$remote_image_name"
 	cp ./pack_me.sh unpack_me.sh $local_install_dir
-	docker run --platform linux/amd64 -it --rm -v $local_install_dir:$container_install_dir "$remote_image_name" $container_install_dir/pack_me.sh "$image_name" "$gemc_only"
+	docker run --platform linux/amd64 -it --rm -v $local_install_dir:$container_install_dir "$remote_image_name" $container_install_dir/pack_me.sh "$image_name" "$what_to_pack"
 }
 
 # clas12tags images
-echo "\n\nClas12Tags images:\n"
-for osname in $=osnames; do
-	for cdocker_tag in $clas12tags_docker_tags; do
-		for install_dir in $install_dirs; do
-			g4_version=$(g4_version_from_clas12tags_docker_version "$cdocker_tag")
-			image_name="$cdocker_tag-g4v$g4_version-$osname-$install_dir"
-			# if image_name contains ubuntu, skip it
-			if [[ $image_name == *"ubuntu"* ]]; then
-				echo "Skipping $image_name"
-				continue
-			fi
-
-			do_the_deed "$image_name" "gemc"
+if [[ "$what_to_pack" = "gemc" || "$what_to_pack" = "all" ]]; then
+	echo "\n\nClas12Tags images:\n"
+	for osname in $=osnames; do
+		for cdocker_tag in $clas12tags_docker_tags; do
+			for install_dir in $install_dirs; do
+				g4_version=$(g4_version_from_clas12tags_docker_version "$cdocker_tag")
+				image_name="$cdocker_tag-g4v$g4_version-$osname-$install_dir"
+				# if image_name contains ubuntu, skip it
+				if [[ $image_name == *"ubuntu"* ]]; then
+					echo "Skipping $image_name"
+					continue
+				fi
+				do_the_deed "$image_name" "gemc"
+			done
 		done
 	done
-done
-
+fi
 
 # remaining g4 versions - only if NOT gemc only
-if [ "$gemc_only" = "" ]; then
+if [[ "$what_to_pack" = "sim" || "$what_to_pack" = "all" ]]; then
 	echo "\n\nRemaining g4 versions:\n"
 	for osname in $=osnames; do
 		for g4_version in $=g4_versions_not_in_clas12; do
 			for install_dir in $=install_dirs; do
 				image_name="g4v$g4_version-$osname-$install_dir"
-				do_the_deed "$image_name" "gemc"
+				do_the_deed "$image_name" "sim"
 			done
 		done
 	done
@@ -78,6 +80,7 @@ fi
 
 echo
 echo "To copy the tar file to the host, may have to remove .tcshrc from jlab then run:"
+echo "rm ~/.tcshrc"
 echo "cd $local_install_dir"
 echo "scp *.tar.gz unpack_me.sh ifarm:/work/clas12/ungaro/images"
 echo
--- pack_me.sh
+++ pack_me.sh
@@ -1,13 +1,15 @@
 #!/bin/zsh
 
 image=$1
-gemc_only=$2
+what_to_pack=$2
 export_location="/usr/local/mywork"
 
 function ce_version_from_clas12tags_docker_version {
-	if [ "$2" = "yes" ]; then
+	if [ "$2" = "gemc" ]; then
 		echo $(find ./ -name clas12Tags)
-#		echo $(find ./ -name clas12Tags) $(find ./ -name ccdb | grep -v bin | grep -v lib | grep -v ceInstall | grep -v python)
+		#		echo $(find ./ -name clas12Tags) $(find ./ -name ccdb | grep -v bin | grep -v lib | grep -v ceInstall | grep -v python)
+	elif [ "$2" = "sim" ]; then
+		echo $(find ./ -name clhep | head -1) $(find ./ -name geant4 | head -1) $(find ./ -name qt | head -1) $(find ./ -name xercesc | head -1)
 	else
 		case $1 in
 			prod1) echo "1.0" ;;
@@ -18,7 +20,7 @@ function ce_version_from_clas12tags_docker_version {
 
 # complementary of the above
 function ce_version_from_g4v_docker_version {
-	if [ "$2" = "yes" ]; then
+	if [[ "$2" = "gemc"  || "$what_to_pack" = "all" ]]; then
 		echo $(find ./ -name clas12Tags)
 	else
 		case $1 in
@@ -29,21 +31,26 @@ function ce_version_from_g4v_docker_version {
 
 docker_tag=$(echo "$image" | cut -d'-' -f1)
 tar_file="$export_location/$image.tar.gz"
-echo ; echo
+echo
+echo
 echo "Image: $image"
 echo "Docker Tag: $docker_tag"
 echo "Tar file: $tar_file"
-if  [ "$gemc_only" = "yes" ]; then
+if  [ "$what_to_pack" = "gemc" ]; then
 	echo "Packing Gemc Only"
+elif  [ "$what_to_pack" = "sim" ]; then
+	echo "Packing Sim Only"
+else
+	echo "Packing All"
 fi
 
 # this conditions will go away when gemc will catch up to g4v versions
 if [ "$docker_tag" = "prod1" ] || [ "$docker_tag" = "dev" ]; then
 	os_tag=$(echo "$image" | cut -d'-' -f3)
-	to_tar="$(ce_version_from_clas12tags_docker_version "$docker_tag" "$gemc_only")"
+	to_tar="$(ce_version_from_clas12tags_docker_version "$docker_tag" "$what_to_pack")"
 else
 	os_tag=$(echo "$image" | cut -d'-' -f2)
-	to_tar="$(ce_version_from_g4v_docker_version "$docker_tag" "$gemc_only")"
+	to_tar="$(ce_version_from_g4v_docker_version "$docker_tag" "$what_to_pack")"
 fi
 
 echo "Os Tag: $os_tag"
@@ -60,10 +67,10 @@ find ./ -type d -name ".git" -exec rm -rf {} +
 echo
 echo Done, now tarring...
 
+echo "tar cvfz $tar_file $=to_tar"
 tar cvfz "$tar_file" "$=to_tar"
 
 # if docker_tag is dev, tar noarch as well
 if [ "$docker_tag" = "dev" ]; then
 	tar cvfz noarch.tar.gz noarch
 fi
-
